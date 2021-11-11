47a1771d0acf663c9b3fcc893f99153f72bb0e7e
mabdellateef <mmabdelateef@gmail.com>
2021-11-11 21:35:48 +0200

    Initial Commit

---
+++ Orbiter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Orbiter
+//
+//  Created by Mostafa Abdellateef on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Orbiter/OrbiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbiterApp.swift
+//  Orbiter
+//
+//  Created by Mostafa Abdellateef on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct OrbiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
